4d607b2a2a500908b8fa6ee7b5a1c113eee581b0
Ben Hohner <benhohner@gmail.com>
2026-06-17 06:34:15 -0400

    Add KaTeX math rendering for markdown posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export default defineConfig({
   site: 'https://inner-lattice.com',
   output: 'static',
+  markdown: {
+    remarkPlugins: [[remarkMath, { singleDollarTextMath: true }]],
+    rehypePlugins: [rehypeKatex],
+  },
 });
