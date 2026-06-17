3a3769b6511a2d43ad67fcaabcc978d3a404fecf
Ben Hohner <benhohner@gmail.com>
2026-06-17 08:09:04 -0400

    Align Shiki code theme with site palette

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,24 @@ export default defineConfig({
   site: 'https://inner-lattice.com',
   output: 'static',
   markdown: {
+    shikiConfig: {
+      theme: {
+        name: 'inner-lattice',
+        type: 'dark',
+        colors: {
+          'editor.background': '#26323a',
+          'editor.foreground': '#edf4f5',
+        },
+        tokenColors: [
+          { scope: ['comment', 'punctuation.definition.comment'], settings: { foreground: '#9fb0b8' } },
+          { scope: ['string', 'constant.other.symbol'], settings: { foreground: '#c7d6b7' } },
+          { scope: ['constant.numeric', 'constant.language'], settings: { foreground: '#e7c889' } },
+          { scope: ['keyword', 'storage', 'entity.name.tag'], settings: { foreground: '#9fc4cf' } },
+          { scope: ['entity.name.function', 'support.function'], settings: { foreground: '#bfd4eb' } },
+          { scope: ['variable', 'entity.name'], settings: { foreground: '#edf4f5' } },
+        ],
+      },
+    },
     remarkPlugins: [[remarkMath, { singleDollarTextMath: true }]],
     rehypePlugins: [rehypeKatex],
   },
